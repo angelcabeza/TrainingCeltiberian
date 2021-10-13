589da483e767c1b0676ad37a2b1b78793d7ebbb8
angelcabeza <angelcabezamartin@gmail.com>
2021-10-13 20:57:54 +0200

    Created some components of Redux and Axio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+import AddBook from './components/add-book.component';
+import Book from './components/book.component';
+import BooksList from './components/books-list.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to={'/books'}>
+          bezKoder
+        </Link>
+        <div>
+          <li>
+            <Link to={'/books'}>
+              Books
+            </Link>
+          </li>
+          <li>
+            <Link to={'/add'}>
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div>
+        <Switch>
+          <Route exact path='/add' component={AddBook}/>
+          <Route path='books/:id' component={Book} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
